695a417f510861a2f53490d72dffb4fe180edb03
imh10054abhishekjoshi <157949438+imh10054abhishekjoshi@users.noreply.github.com>
2024-01-28 14:54:14 +0530

    Add files via upload

---
+++ asoneques2/src/asoneques2/Asoneques2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asoneques2;
+
+
+public class Asoneques2 { 
+
+    
+    public static void main(String[] args) {
+        if(args.length > 0){
+            System.out.println("The message from command line: " + args[0]);
+        } else{
+            System.out.println("No message was provided. Pass the message as a command line argument.");
+        }
+    }
+    
+}
